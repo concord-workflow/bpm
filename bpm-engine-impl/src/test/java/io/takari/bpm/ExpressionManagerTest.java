d880f1af40af297a596d252f3e3d785e60ef6886
Ivan Bodrov <ibodrov@gmail.com>
2016-07-07 15:56:38 -0400

    more juel tests

--- bpm-engine-impl/src/test/java/io/takari/bpm/ExpressionManagerTest.java
+++ bpm-engine-impl/src/test/java/io/takari/bpm/ExpressionManagerTest.java
@@ -11,10 +11,29 @@ import static org.mockito.Mockito.*;
 public class ExpressionManagerTest {
 
     @Test
+    public void testMethodCall() throws Exception {
+        ServiceTaskRegistry registry = mock(ServiceTaskRegistry.class);
+        when(registry.getByKey(anyString())).thenReturn(new TestBean());
+        
+        ExpressionManager em = new DefaultExpressionManager(registry);
+        String expr = "${bean.sayHello('world')}";
+        String result = em.eval(mock(ExecutionContext.class), expr, String.class);
+        
+        assertEquals("Hello, world!", result);
+    }
+    
+    @Test
     public void testEvalString() throws Exception {
         ExpressionManager em = new DefaultExpressionManager(mock(ServiceTaskRegistry.class));
         String s1 = "PT30S";
         String s2 = em.eval(mock(ExecutionContext.class), s1, String.class);
         assertEquals(s1, s2);
+    }
+    
+    public static final class TestBean {
+        
+        public String sayHello(String s) {
+            return "Hello, " + s + "!";
+        }
     }
 }
